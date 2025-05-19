41e13646ef6017a57f31adbd58f4b5e7a3aa8c49
yogesh nikam <yogesh.nikam@siemens.com>
2025-05-19 21:45:14 +0200

    Basic agent

---
+++ LangGraph/basic_react_agent.py
@@ -0,0 +1,30 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.agents import initialize_agent, tool
+from langchain_community.tools import TavilySearchResults
+import datetime
+
+load_dotenv()
+
+# Initialize the Google Generative AI model
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+# Initialize the Tavily search tool
+search_tool = TavilySearchResults(seach_depth="basic")
+
+@tool
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S" ):
+    """  Get the current system time in the specified format. """
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime(format)
+    return formatted_time
+
+tools = [search_tool, get_system_time]
+# Initialize the agent with the Google Generative AI model and the search tool
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent_type="zero-shot-react-description",
+    verbose=True)
+
+agent.invoke("When the Chandrayaan-3 was launch and how many days ago was the from this instant?") 
