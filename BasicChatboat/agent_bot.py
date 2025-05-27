822ad54ff0f0b9256ff3da6e114cd92ed3d67ada
yogesh nikam <yogesh.nikam@siemens.com>
2025-05-27 15:44:05 +0200

    Initial of a agent

---
+++ BasicChatboat/agent_bot.py
@@ -0,0 +1,39 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Define the AgentState TypedDict to represent the state of the agent
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+    
+# Initialize llm with OpenAI's Chat model
+llm = ChatOpenAI(model="gpt-4o")
+
+# Define the process function that will be used in the state graph
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    return state
+
+# Initialize the state graph
+stateGraph = StateGraph(AgentState)
+
+# Add the process function as a node in the graph
+stateGraph.add_node("process", process)
+
+# Add edges to connect the START node to the process node and the process node to the END node
+stateGraph.add_edge(START, "process")
+stateGraph.add_edge("process", END)
+
+# Compile the graph
+agent = stateGraph.compile()
+
+
+user_input = input("You: ")
+agent.invoke({"messages": [HumanMessage(content=user_input)]})
+
